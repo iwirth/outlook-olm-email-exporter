65a87809e59fed7064014f9b286a2f7e060c5eca
SidneyCodes <68404970+SidneyCodes@users.noreply.github.com>
2022-04-26 17:39:53 -0400

    Update using glob

--- extract.py
+++ extract.py
@@ -1,41 +1,58 @@
 import os
 import xml.etree.ElementTree as ET
+import glob
 
 folders = ["Inbox", "Sent Items"]
 blacklist = ["asana.com", "reply", "noreply", "registration", "chime", "support@", "mailchimp", ".calendar", "=", "-_", "mailbox", "+","account","fatura","hizmetleri","musteri","marketing@", "help@", "sales"]
 
-email_list = []
-for folder in folders:
-    print("> Looking for: "+folder)
-    current_folder = "Local/com.microsoft.__Messages/"+folder
-    for filename in os.listdir(current_folder):
-        if not filename.endswith(".xml"):
-            print(current_folder+"/"+filename+" is not xml")
-            continue
+print("Starting to read file")
+"""
+1. Export your all e-mails using Export menu in Outlook.
+2. Go to terminal and use `unzip Outlook\ for\ Mac\ Archive.olm` to unzip the archive file.
+3. Put the `extract.py` file near `Local` directory.
+4. Run `python extract.py` and obtain the emails.txt file
+5. change the path below to match the folder with your extracted .olm file
+"""
+files = glob.glob('/Users/data/Desktop/emailextract/**/*.xml', 
+                   recursive = True)
 
-        try:
-            tree = ET.parse(current_folder+"/"+filename)
-        except:
-            print(current_folder+"/"+filename+" couldn't parsed")
-        finally:
-            root = tree.getroot()
-            for item in root.iter('emailAddress'):
-                if 'OPFContactEmailAddressAddress' in item.attrib:
-                    if any(to_check in item.attrib['OPFContactEmailAddressAddress'].lower() for to_check in blacklist):
-                        #print(">>"+item.attrib['OPFContactEmailAddressAddress']+" is blacklisted")
-                        continue
-                    if any(item.attrib['OPFContactEmailAddressAddress'].lower() in s for s in email_list):
-                        #print(">>"+item.attrib['OPFContactEmailAddressAddress']+" exists")
-                        continue
-                    if 'OPFContactEmailAddressName' in item.attrib:
-                        to_append = item.attrib['OPFContactEmailAddressName']+" <"+item.attrib['OPFContactEmailAddressAddress'].lower()+">"
-                    else:
-                        to_append = item.attrib['OPFContactEmailAddressAddress']+" <"+item.attrib['OPFContactEmailAddressAddress'].lower()+">"
-                    email_list.append(to_append)
+email_list = []
+
+for file in files:
+    #print(file)
+    #print(len(files))
+
+    if not file.endswith(".xml"):
+        print(file + " is not xml")
+        continue
+    try:
+        tree = ET.parse(file)
+    except:
+        print(file + " couldn't parsed")
+    finally:
+        root = tree.getroot()
+        for item in root.iter('emailAddress'):
+            if 'OPFContactEmailAddressAddress' in item.attrib:
+                if any(to_check in item.attrib['OPFContactEmailAddressAddress'].lower() for to_check in blacklist):
+                    #print(">>"+item.attrib['OPFContactEmailAddressAddress']+" is blacklisted")
+                    continue
+                if any(item.attrib['OPFContactEmailAddressAddress'].lower() in s for s in email_list):
+                    #print(">>"+item.attrib['OPFContactEmailAddressAddress']+" exists")
+                    continue
+                if 'OPFContactEmailAddressName' in item.attrib:
+                    to_append = item.attrib['OPFContactEmailAddressName']+" <"+item.attrib['OPFContactEmailAddressAddress'].lower()+">"
+                else:
+                    to_append = item.attrib['OPFContactEmailAddressAddress']+" <"+item.attrib['OPFContactEmailAddressAddress'].lower()+">"
+                email_list.append(to_append)
 
 # make every email unique
 email_list = list(set(email_list))
+#print(email_list)
 
-f = open("export.txt", "w")
-f.write("\n".join(email_list).encode('utf8'))
-print("operation completed")
+
+textfile = open("emails.txt", "w")
+for element in email_list:
+    textfile.write(element + "\n")
+textfile.close()
+
+#https://github.com/eercanayar/outlook-olm-email-exporter/blob/master/extract.py
